a4174a2b9ec488d2b3ce8478a7b78818664d2a86
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2024-06-27 13:54:29 +0200

    exclude index.html

--- generate_html.py
+++ generate_html.py
@@ -18,7 +18,7 @@ _FOLDERICON = "https://www.svgrepo.com/show/400249/folder.svg"
 _ROOTTITLE = "Pictures"
 imgext = [".jpg", ".jpeg"]
 rawext = [".3fr", ".ari", ".arw", ".bay", ".braw", ".crw", ".cr2", ".cr3", ".cap", ".data", ".dcs", ".dcr", ".dng", ".drf", ".eip", ".erf", ".fff", ".gpr", ".iiq", ".k25", ".kdc", ".mdc", ".mef", ".mos", ".mrw", ".nef", ".nrw", ".obm", ".orf", ".pef", ".ptx", ".pxn", ".r3d", ".raf", ".raw", ".rwl", ".rw2", ".rwz", ".sr2", ".srf", ".srw", ".tif", ".tiff", ".x3f"]
-excludes = ["Galleries", ".previews", "Archives", "Wallpaper"]
+excludes = ["index.html", "Galleries", ".previews", "Archives", "Wallpaper"]
 
 thumbnails: list[tuple[str, str]] = []
 
