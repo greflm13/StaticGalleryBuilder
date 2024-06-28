464625119d1ca04849f6617d98e266816c59a0c9
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2024-06-28 11:55:56 +0200

    forgot foldericon

--- generate_html.py
+++ generate_html.py
@@ -126,6 +126,7 @@ def listfolder(folder: str, title: str):
                 root=args.webroot,
                 parent=parent,
                 header=header,
+                foldericon=args.foldericon
                 license=_license,
                 subdirectories=subfolders,
                 images=imagechunks,
